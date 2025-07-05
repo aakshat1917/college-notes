a90661ce042dfc2b7868212be8a8a49dc93464d3
Akshat Singh <akshat1917.as@gmail.com>
2025-07-05 12:27:52 +0530

    Final commit for deployment

--- notes_marketplace/settings.py
+++ notes_marketplace/settings.py
@@ -62,12 +62,13 @@ WSGI_APPLICATION = 'notes_marketplace.wsgi.application'
 
 
 # Database (default: SQLite3)
+import dj_database_url
+import os
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=os.environ.get("DATABASE_URL"))
 }
+
 
 
 # Password validation
