517146013646ff82f8589c9c5713c29cb2bb7db7
Akshat Singh <akshat1917.as@gmail.com>
2025-07-05 13:03:09 +0530

    Rechecking deployment files

--- build.sh
+++ build.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-# Install dependencies
 pip install -r requirements.txt
-
-# Run migrations
 python manage.py migrate
-
-# Create superuser only if it doesn't exist (optional)
 echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')" | python manage.py shell
